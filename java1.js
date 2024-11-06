b17660fae8f2a270d155088a67fef9dd24ec50ac
Anuya-10 <anushkayadav9e.gr1@gmail.com>
2024-11-06 23:41:59 +0530

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,10 @@
+ document.querySelector("button").addEventListener("click",function(a){
+    a.preventDefault();
+    const name=document.getElementsByid("inputbox1").value;
+    const current=document.getElementsByid("inputbox2").value;
+
+    console.log(alert("welocome"));
+    
+    console.log("current location",current);
+    document.getElementById();
+}); 
