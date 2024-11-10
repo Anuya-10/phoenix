b93a1b5ebabc7c538a3a92c86637b92051b34b93
stuti252 <stutiag5126@gmail.com>
2024-11-10 09:42:48 +0530

    Add files via upload

--- java1.js
+++ java1.js
@@ -1,9 +1,10 @@
  document.querySelector("button").addEventListener("click",function(a){
     a.preventDefault();
-    const name=document.getElementsByid("inputbox1").value;
-    const current=document.getElementsByid("inputbox2").value;
-
-    console.log(alert("welocome"));
+    let phone=document.getElementById("inputbox1").value;
+    let user=document.getElementById("inputbox2").value;
+    
+    alert("welcome "+user);
+    window.location.href = "page4.html";
     
     console.log("current location",current);
     document.getElementById();
---
+++ java3.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click",function(a){
+    a.preventDefault();
+    let user=document.getElementById("inputbox1").value;
+        
+    alert("welcome "+user);
+    window.location.href = "page4.html";
+    console.log("current location",current);
+    document.getElementById();
+}); 
